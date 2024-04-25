badab8d5e9aa748f97808a1213c832a097381ec9
Anusree-mj <anuanusree682@gmail.com>
2024-04-25 16:46:43 +0530

    did final check

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Anusree MJ",
-  profile: '/profile.png',
+  // profile: '/profile.jpeg',
   designation: "Software Developer",
   description: "My name is Anusree. I am a professional and enthusiastic programmer in my day-to-day work. I am a quick learner with a proactive attitude towards self-learning. I love to learn and explore new technologies and am passionate about problem-solving. I strive to make my web applications more accessible to the world. My core skill is based on the MERN Stack, and I prefer to work primarily with JavaScript/TypeScript. I am available for any job opportunity that aligns with my skills and interests.",
   email: 'anu.mj0608@gmail.com',
