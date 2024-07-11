8656c26d8e21bd0f8ed3bdc59429a3534108d1a5
Anusree-mj <anuanusree682@gmail.com>
2024-07-12 00:01:10 +0530

    change resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -10,5 +10,5 @@ export const personalData = {
   linkedIn: 'https://www.linkedin.com/in/anusree-m-j-003756286/',
   leetcode: "https://leetcode.com/AnusreeMJ/",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/1ow-bQDixQmTdIihMZ636TtHeIlsgqLE9/view?usp=sharing"
+  resume: "https://docs.google.com/document/d/1vvMMRnmGwinAG2MyO0-g0ZrSPmCkeNKg/view"
 }
